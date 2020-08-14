143d4b61e024fb76bf399cc38f3520d9578eb394
samirboulil <contact@akeneo.com>
2020-08-14 14:05:13 +0000

    Update from https://github.com/akeneo/pim-community-dev/commit/904d3a5417e18d4938009cb8c0007a8405af4f7d

--- src/components/Dummy/Dummy.unit.tsx
+++ src/components/Dummy/Dummy.unit.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import {Dummy} from './Dummy';
+// TODO check this import '@testing-library/jest-dom/extend-expect';
 
 it('has a href attribute when rendering with linkWrapper', () => {
   const {container, getByText} = render(<Dummy>Nice</Dummy>);
 
   expect(container.firstChild).not.toBeNull();
-  expect(getByText('Nice')).toBeInTheDocument();
+  expect(getByText('Nice')).not.toBeNull();
 });
